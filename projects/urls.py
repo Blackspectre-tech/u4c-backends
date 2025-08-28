56c2d0305b8c840fbd8b3f278ce72531f2f543be
steve <stevechris179@gmail.com>
2025-08-28 18:34:49 +0100

    cors3

--- projects/urls.py
+++ projects/urls.py
@@ -15,16 +15,16 @@ from .views import (
 )
 
 urlpatterns = [
-    path('create/', ProjectCreateView.as_view()),
+    path('/create', ProjectCreateView.as_view()),
     path('', listApprovedProjectsView.as_view()),
-    path('<int:pk>/', RetrieveProjectsView.as_view()),
-    path('organization/<int:pk>/', listOrgProjectsView.as_view()),
-    path('<int:pk>/comments', listCommentsByProjectsView.as_view()),
-    path('<int:pk>/comments/add/', CommentCreateView.as_view()),
-    path('<int:pk>/post-update/', PostUpdateView.as_view()),
-    path('comments/<int:pk>', CommentRetrieveUpdateDestroyView.as_view()),
-    path('milestones/<int:pk>/', MilestoneRetrieveView.as_view()),
-    path('milestones/<int:pk>/post-images/', PostMilestoneImages.as_view()),
-    path('milestones/<int:pk>/add-expenses', ExpensesCreateView.as_view()),
-    path('milestones/images/<int:pk>', MilestoneImagesRetrieveUpdateDestroyAPIView.as_view()),
+    path('/<int:pk>', RetrieveProjectsView.as_view()),
+    path('/organization/<int:pk>', listOrgProjectsView.as_view()),
+    path('/<int:pk>/comments', listCommentsByProjectsView.as_view()),
+    path('/<int:pk>/comments/add', CommentCreateView.as_view()),
+    path('/<int:pk>/post-update', PostUpdateView.as_view()),
+    path('/comments/<int:pk>', CommentRetrieveUpdateDestroyView.as_view()),
+    path('/milestones/<int:pk>', MilestoneRetrieveView.as_view()),
+    path('/milestones/<int:pk>/post-images', PostMilestoneImages.as_view()),
+    path('/milestones/<int:pk>/add-expenses', ExpensesCreateView.as_view()),
+    path('/milestones/images/<int:pk>', MilestoneImagesRetrieveUpdateDestroyAPIView.as_view()),
 ]

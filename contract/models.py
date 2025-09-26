7f8aa2f6bf7c2abbd8de667a5aa6e0d6196ce5ef
steve <stevechris179@gmail.com>
2025-09-26 03:23:28 +0100

    26

--- contract/models.py
+++ contract/models.py
@@ -6,4 +6,4 @@ class ContractLog(models.Model):
     data = models.TextField()
     error = models.TextField(null=True, blank=True)
     notes = models.TextField(null=True, blank=True)
-    time = models.DateTimeField(auto_now_add=True)
+    time = models.DateTimeField(auto_now_add=True, blank=True, null=True)
